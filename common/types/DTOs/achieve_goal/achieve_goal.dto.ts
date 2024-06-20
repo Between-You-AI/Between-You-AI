d73968761562ee1f76b5cae39b11ad981fd472ee
root <root@LAPTOP-32OIK6GL.>
2024-06-19 23:27:27 -0500

    chore(models): chore add models schema to models.yml

---
+++ common/types/DTOs/achieve_goal/achieve_goal.dto.ts
@@ -0,0 +1,16 @@
+interface FindExpertDTO {
+  objective: string;
+}
+
+interface FindExpertDTO {
+  expertName: string;
+  details: string;
+}
+
+interface Task {
+  id: number;
+  name: string;
+  objective: string;
+  duration: string;
+  budget: string;
+}
