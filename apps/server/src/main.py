e27685f2538bc5769bcaaca20a607169093b44c5
root <root@LAPTOP-32OIK6GL.>
2024-06-26 14:40:32 -0500

    feat(websocket): add websocket connection for frontend and backend

--- apps/server/src/main.py
+++ apps/server/src/main.py
@@ -1,3 +1,4 @@
+import json
 import sys
 import os
 
@@ -6,12 +7,15 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../.
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../../../apps')))
 
 
+import time
 from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
 from experts.service import ExpertService
+from .utils.websocket_manager import WebSocketManager
 
 app = FastAPI()
 
+manager = WebSocketManager()
 
 @app.get("/")
 def read_root():
@@ -27,3 +31,25 @@ async def get_agent(query: str):
     results = await ExpertService(query).find_expert()
     print(results)
     return results
+
+@app.websocket("/ws")
+async def websocket_endpoint(websocket: WebSocket):
+    await manager.connect(websocket)
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(data)
+            websocket.sender_tasks = [{type: 'tasks', data: {'status': 'working'}}]
+            if data.startswith("start"):
+                json_data = json.loads(data[6:])
+                task = json_data.get("task")
+                report_type = json_data.get("report_type")
+                filename = f"task_{int(time.time())}_{task}"
+                report_source = json_data.get("report_source")
+                if task and report_type:
+                    report = await manager.start_streaming(task, report_type, report_source, websocket)
+                else:
+                    print("Error: not enough parameters provided.")
+
+    except WebSocketDisconnect:
+        await manager.disconnect(websocket)
